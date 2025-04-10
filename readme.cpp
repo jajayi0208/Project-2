a705902d9cbd603121f7d2794b543dc24f8c6613
jajayi0208 <129896718+jajayi0208@users.noreply.github.com>
2025-04-10 11:38:59 -0400

    Update readme.cpp

--- readme.cpp
+++ readme.cpp
@@ -2,21 +2,19 @@ Project2 Decimal to Binary Conversion
 
 This project is for converting a decimal number into its binary equivalent using a stack for the integer part and a queue for the decimal part.
 
-1. The program first asks the user to input a decimal number.
-2. It then splits the number into two parts
-    Decimal part 
-    Integer part 
-3. The integer part is converted to binary using a stack.
-4. The decimal part is converted to binary using a queue.
-5. The binary representations are printed out.
+ The program first asks the user to input a decimal number.
+ It then splits the number into two parts, Decimal and Integer. 
+ The integer part is converted to binary using a stack.
+ The decimal part is converted to binary using a queue.
+ The binary representations are printed out.
 
-Files
-1. Stack.h - Header file for the Stack class
-2. stack.cpp - Implementation of the Stack class
-3. Queue.h - Header file for the Queue class
-4. queue.cpp - Implementation of the Queue class
-5. Main.cpp - Main program that performs the decimal to binary conversion
-6. Readme.txt - Information about the project
+ Files
+ Stack.h - Header file for the Stack class
+ stack.cpp - Implementation of the Stack class
+ Queue.h - Header file for the Queue class
+ queue.cpp - Implementation of the Queue class
+ Main.cpp - Main program 
+ Readme.txt - Information about the project
 
 
 
